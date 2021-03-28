80e020fbd8a7359e6567669ac05b9fd697d22145
leeyjeen <leeyjeen@naver.com>
2021-03-28 23:15:11 +0900

    problem-solving: adventofcode 2020 day1 in c++

---
+++ problem-solving/adventofcode2020/day1.cpp
@@ -0,0 +1,50 @@
+// https://adventofcode.com/2020/day/1
+#include <iostream>
+#include <string>
+#include <vector>
+#include <fstream>
+
+using namespace std;
+
+vector<int> readInput(string filepath) {
+    vector<int> inputList;
+    ifstream inputFile;
+    inputFile.open(filepath);
+    
+    int inputValue;
+    while (inputFile >> inputValue) {
+        inputList.push_back(inputValue);
+    }
+    inputFile.close();
+    return inputList;
+}
+
+int solveFirst(vector<int> numbers) {
+    for (int i=0; i<numbers.size(); i++) {
+        for (int j=i+1; j<numbers.size(); j++) {
+            if (numbers[i] + numbers[j] == 2020) {
+                return numbers[i]*numbers[j];
+            }
+        }
+    }
+    return 0;
+}
+
+int solveSecond(vector<int> numbers) {
+    for (int i=0; i<numbers.size(); i++) {
+        for (int j=i+1; j<numbers.size(); j++) {
+            for (int k=j+1; k<numbers.size(); k++) {
+                if (numbers[i] + numbers[j] + numbers[k] == 2020) {
+                    return numbers[i]*numbers[j]*numbers[k];
+                }
+            }
+        }
+    }
+    return 0;
+}
+
+int main() {
+    vector<int> inputList = readInput("./day1.txt");
+    cout << solveFirst(inputList) << endl;
+    cout << solveSecond(inputList) << endl;
+}
