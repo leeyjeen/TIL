305b441ba30d31c298d8dffb2638f949483b5bfa
leeyjeen <leeyjeen@naver.com>
2021-03-29 22:15:55 +0900

    problem-solving: adventofcode 2020 day3 in c++

---
+++ problem-solving/adventofcode2020/day3.cpp
@@ -0,0 +1,51 @@
+// https://adventofcode.com/2020/day/3
+#include <iostream>
+#include <string>
+#include <vector>
+#include <fstream>
+#include <cstring>
+
+using namespace std;
+
+const char TREE = '#';
+
+vector<string> readInput(string filepath) {
+    vector<string> inputList;
+    ifstream inputFile;
+    inputFile.open(filepath);
+
+    string line;
+    while (inputFile >> line) {
+        inputList.push_back(line);
+    }
+    inputFile.close();
+    return inputList;
+}
+
+int getTreeCount(int rightInterval, int downInterval, vector<string> inputList) {
+    int row = 0;
+    int col = 0;
+    int treeCount = 0;
+    while (row+downInterval < inputList.size()) {
+        col = (col+rightInterval) % inputList[0].size();
+        row += downInterval;
+        if (inputList[row][col] == TREE) {
+            treeCount++;
+        }
+    }
+    return treeCount;
+}
+
+int solveFirst(vector<string> inputList) {
+    return getTreeCount(3, 1, inputList);
+}
+
+int solveSecond(vector<string> inputList) {
+    return getTreeCount(1, 1, inputList)*getTreeCount(3, 1, inputList)*getTreeCount(5, 1, inputList)*getTreeCount(7, 1, inputList)*getTreeCount(1, 2, inputList);
+}
+
+int main() {
+    vector<string> inputList = readInput("./day3.txt");
+    cout << solveFirst(inputList) << endl;
+    cout << solveSecond(inputList) << endl;
+}
