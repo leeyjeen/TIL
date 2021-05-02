2fa4eef1f055e3addd8402ec67eeb6751596afef
leeyjeen <leeyjeen@naver.com>
2021-05-03 01:04:06 +0900

    problem-solving: adventofcode 2020 day13 part2 in python3 - need to be improved

--- problem-solving/adventofcode2020/day13.py
+++ problem-solving/adventofcode2020/day13.py
@@ -1,10 +1,11 @@
 # https://adventofcode.com/2020/day/13
 import sys
+from collections import defaultdict
 
 with open('./day13.txt', 'r') as f:
-    depart_time_stamp, buses = f.read().splitlines()
+    depart_time_stamp, input_buses = f.read().splitlines()
     depart_time_stamp = int(depart_time_stamp)
-    bus_set = set(buses.split(","))
+    bus_set = set(input_buses.split(","))
     bus_set.discard("x")
     bus_list = list(map(int, bus_set))
     bus_list.sort()
@@ -22,5 +23,24 @@ def solve_part1():
     return (min_time_stamp-depart_time_stamp) * bus_number
 
 
+def solve_part2():
+    bus_numbers = [(i, int(bus))
+                   for i, bus in enumerate(input_buses.split(",")) if bus != "x"]
+    earliest_time_stamp = 0
+    while True:
+        is_valid = True
+        for i in range(1, len(bus_numbers)):
+            bus_tuple = bus_numbers[i]
+            if (earliest_time_stamp + bus_tuple[0]) % bus_tuple[1] != 0:
+                earliest_time_stamp += bus_numbers[0][1]
+                is_valid = False
+                break
+        if is_valid:
+            break
+
+    return earliest_time_stamp
+
+
 if __name__ == "__main__":
     print(solve_part1())
+    print(solve_part2())
