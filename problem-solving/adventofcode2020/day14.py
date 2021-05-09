783baecdf8cde2d6e17b13180630bd3668dcc21b
leeyjeen <leeyjeen@naver.com>
2021-05-10 03:20:29 +0900

    problem-solving: adventofcode 2020 day14 part2 in python3

--- problem-solving/adventofcode2020/day14.py
+++ problem-solving/adventofcode2020/day14.py
@@ -2,6 +2,15 @@
 with open('./day14.txt', 'r') as f:
     lines = f.read().splitlines()
 
+"""
+PART 1:
+
+mask 0 -> change to 0
+mask 1 -> change to 1
+mask X -> do not change
+value update..
+"""
+
 
 def apply_masking(mask, val):
     val_after_masking = ""
@@ -27,5 +36,56 @@ def solve_part1(lines):
     return sum(memory.values())
 
 
+"""
+PART 2:
+
+[modifies memory address]
+mask 0 -> do not change
+mask 1 -> overwrite with 1
+mask X -> take on all possible values...!
+주소를 먼저 변환한 후, 변환한 주소들에 값을 매핑.
+
+answer: sum of all values left in memory after it completes
+"""
+
+
+def apply_masking2(mask, addr, val):
+    bin_addr = bin(addr)[2:].zfill(36)
+    values_after_masking = dict({"": val})  # 빈 문자열인 key로 초기화
+    for m, a in zip(mask, bin_addr):
+        if m == "0":    # 각 key 값에 addr 값 더해서 업데이트
+            new_values_after_masking = dict()
+            for v in values_after_masking:
+                new_values_after_masking[v+a] = val
+            values_after_masking = new_values_after_masking
+        elif m == "1":  # 각 key 값에 "1" 값 더해서 업데이트
+            new_values_after_masking = dict()
+            for v in values_after_masking:
+                new_values_after_masking[v+"1"] = val
+            values_after_masking = new_values_after_masking
+        elif m == "X":  # 각 key 값에 모든 경우 ("0", "1") 값 더해서 업데이트
+            new_values_after_masking = dict()
+            for v in values_after_masking:
+                new_values_after_masking[v+"0"] = val
+                new_values_after_masking[v+"1"] = val
+            values_after_masking = new_values_after_masking
+    return values_after_masking
+
+
+def solve_part2(lines):
+    addresses = dict()
+    mask = ""
+    for line in lines:
+        splited = line.split(" = ")
+        if splited[0] == "mask":
+            mask = splited[1]
+        else:
+            addr = int(splited[0].split("[")[1].split("]")[0])
+            val = int(splited[1])
+            addresses.update(apply_masking2(mask, addr, val))
+    return sum(addresses.values())
+
+
 if __name__ == "__main__":
     print(solve_part1(lines))
+    print(solve_part2(lines))
